4eeb16fe1a1aef1e7018f95f686b0ec2380bb50c
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-08-31 11:48:38 +0200

    Fix XO warnings

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 'use strict';
 module.exports = function (red, green, blue) {
-	if (typeof red === 'object') {
-		var color = red;
-	} else {
-		var color = {
-			red: red,
-			green: green,
-			blue: blue
-		};
-	}
+	var color = typeof red === 'object' ? red : {
+		red: red,
+		green: green,
+		blue: blue
+	};
 
 	return 'rgb(' + color.red + ', ' + color.green + ', ' + color.blue + ')';
 };
