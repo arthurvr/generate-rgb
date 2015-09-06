0ab930a68d90e3cfe1d787b36a20aba0152bc893
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-09-06 21:41:24 +0200

    Use AVA for testing

--- test.js
+++ test.js
@@ -1,15 +1,18 @@
 'use strict';
-var assert = require('assert');
+var test = require('ava');
 var rgb = require('./');
 
-it('generates an rgb(red, green, blue) string', function () {
-	assert.strictEqual(rgb(100, 100, 100), 'rgb(100, 100, 100)');
+test('generates an rgb(red, green, blue) string', function (t) {
+	t.is(rgb(100, 100, 100), 'rgb(100, 100, 100)');
+	t.end();
 });
 
-it('Also takes an object', function () {
-	assert.strictEqual('rgb(100, 200, 10)', rgb({
+test('Also takes an object', function (t) {
+	t.is('rgb(100, 200, 10)', rgb({
 		red: 100,
 		green: 200,
 		blue: 10
 	}));
+
+	t.end();
 });
