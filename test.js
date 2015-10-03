747d2a2bed6944478266a224e9bb484b70b3892f
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-10-03 22:30:30 +0200

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,13 +1,12 @@
-'use strict';
-var test = require('ava');
-var rgb = require('./');
+import test from 'ava';
+import rgb from './';
 
-test('generates an rgb(red, green, blue) string', function (t) {
+test('generates an rgb(red, green, blue) string', t => {
 	t.is(rgb(100, 100, 100), 'rgb(100, 100, 100)');
 	t.end();
 });
 
-test('Also takes an object', function (t) {
+test('Also takes an object', t => {
 	t.is('rgb(100, 200, 10)', rgb({
 		red: 100,
 		green: 200,
